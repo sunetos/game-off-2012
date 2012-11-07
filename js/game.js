ef8fe83207203efca8afc0d40bac459a09646808
Adam R. Smith <adam@adamia.com>
2012-11-07 14:45:34 -0800

    Rebuilds.

--- js/game.js
+++ js/game.js
@@ -1,18 +1,67 @@
-/// <reference path="libs/jquery.ts" />
+var Msg;
+(function (Msg) {
+    var $elem = $({
+    });
+    function sub(topic, cb) {
+        var _this = this;
+        $elem.on(topic, function () {
+            return cb.apply(_this, Array.prototype.slice.call(arguments, 1));
+        });
+    }
+    Msg.sub = sub;
+    ; ;
+    function unsub() {
+        var args = [];
+        for (var _i = 0; _i < (arguments.length - 0); _i++) {
+            args[_i] = arguments[_i + 0];
+        }
+        $elem.off.apply($elem, args);
+    }
+    Msg.unsub = unsub;
+    ; ;
+    function pub() {
+        var args = [];
+        for (var _i = 0; _i < (arguments.length - 0); _i++) {
+            args[_i] = arguments[_i + 0];
+        }
+        $elem.trigger.apply($elem, args);
+    }
+    Msg.pub = pub;
+    ; ;
+})(Msg || (Msg = {}));
 
-class Cell {
-  constructor(public kind:string) {
-    this.$elem = $('<div class="cell"></div>');
-  }
-}
-
-class CellGrid {
-  cells: Cell[];
-  constructor(public name:string, elem:string) {
-    this.$elem = $(elem);
-  }
-}
-
-class Body {
-  grids: CellGrid[];
-}
+var Cell = (function () {
+    function Cell(kind) {
+        this.kind = kind;
+        this.$elem = $('<div class="cell"></div>');
+    }
+    return Cell;
+})();
+var CellGrid = (function () {
+    function CellGrid(name, elem) {
+        this.name = name;
+        this.$elem = $(elem);
+    }
+    return CellGrid;
+})();
+var Body = (function () {
+    function Body(elem) {
+        var _this = this;
+        this.grids = {
+        };
+        this.$elem = $(elem);
+        this.$elem.find('section').each(function (i, v) {
+            var grid = new CellGrid($(v).data('name'), v);
+            _this.grids[grid.name] = grid;
+        });
+    }
+    return Body;
+})();
+var Game = (function () {
+    function Game(elem) {
+        this.$elem = $(elem);
+        this.body = new Body(this.$elem.find('.body'));
+        Msg.pub('game-init', this);
+    }
+    return Game;
+})();
